7bd2a6502678198402ff005512592b9d4b76a5dd
Rohitsah6372 <91113994+Rohitsah6372@users.noreply.github.com>
2025-05-11 00:49:53 +0530

    Placed the logo cine-searcher

--- src/components/commons/Header.jsx
+++ src/components/commons/Header.jsx
@@ -1,3 +1,9 @@
-const Header = () => <div />;
+import { Typography } from "neetoui";
+
+const Header = () => (
+  <Typography className="px-2 font-bold" style="h2">
+    <span className="text-blue-600">Cine </span>Searcher
+  </Typography>
+);
 
 export default Header;
