0529fc3a44f37fd347c9d4064836b15e6799fd15
Rohitsah6372 <91113994+Rohitsah6372@users.noreply.github.com>
2025-05-11 01:42:05 +0530

    Display movie on search

---
+++ src/components/MovieCard/index.jsx
@@ -0,0 +1,36 @@
+import { Button, Typography } from "neetoui";
+import { useTranslation } from "react-i18next";
+
+import Image from "../commons/Image";
+
+const MovieCard = ({ movie }) => {
+  const { t } = useTranslation();
+
+  const { title, year, poster, type } = movie;
+
+  return (
+    <div className="">
+      <div
+        className="bg-white-900 flex w-56
+      flex-col items-center  justify-between rounded-xl p-4 shadow-2xl"
+      >
+        <div className="h-1/2 px-4">
+          <Image {...{ title, poster }} />
+        </div>
+        <Typography className="inline-block w-40 truncate text-center font-bold">
+          {title}
+        </Typography>
+        <Typography className="text-gray-500">
+          {type === "movie" ? t("movie") : t("series")} - {year}
+        </Typography>
+        <Button
+          className="neeto-ui-cursor-pointer neeto-ui-text-center  neeto-ui-shadow-sm m-1 text-blue-700"
+          label={t("movieDetails")}
+          style="tertiary"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 
+import initializeAxios from "apis/axios";
 import ReactDOM from "react-dom/client";
+import { QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import queryClient from "utils/queryClient";
 
 import App from "./App";
+import "./common/i18n";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
+initializeAxios();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
