9f61eb316e67cd098928ec28dc4390485acafbc2
Rohitsah6372 <91113994+Rohitsah6372@users.noreply.github.com>
2025-05-12 01:07:55 +0530

    Implemented trans component

--- src/components/commons/Header.jsx
+++ src/components/commons/Header.jsx
@@ -1,34 +1,33 @@
 import { Typography } from "neetoui";
-import { useTranslation } from "react-i18next";
+import { Trans } from "react-i18next";
 import { NavLink } from "react-router-dom";
 import routes from "routes";
 
-const Header = () => {
-  const { t } = useTranslation();
-
-  return (
-    <div className="flex items-center">
-      <Typography className="px-2 font-bold" style="h2">
-        <span className="text-blue-600">Cine </span>Searcher
-      </Typography>
-      <NavLink
-        exact
-        activeClassName="px-2 font-semibold text-blue-600"
-        className="px-2 text-black"
-        to={routes.root}
-      >
-        {t("home")}
-      </NavLink>
-      <NavLink
-        exact
-        activeClassName="px-2 font-semibold text-blue-500"
-        className="px-2 text-black"
-        to={routes.favourite}
-      >
-        {t("favourites")}
-      </NavLink>
-    </div>
-  );
-};
+const Header = () => (
+  <div className="flex items-center">
+    <Typography className="px-2 font-bold" style="h2">
+      <span className="text-blue-600">
+        <Trans>cine</Trans>
+      </span>
+      <Trans>searcher</Trans>
+    </Typography>
+    <NavLink
+      exact
+      activeClassName="px-2 font-semibold text-blue-600"
+      className="px-2 text-black"
+      to={routes.root}
+    >
+      <Trans>home</Trans>
+    </NavLink>
+    <NavLink
+      exact
+      activeClassName="px-2 font-semibold text-blue-500"
+      className="px-2 text-black"
+      to={routes.favourite}
+    >
+      <Trans>favourites</Trans>
+    </NavLink>
+  </div>
+);
 
 export default Header;
--- src/components/commons/PageNotFound.jsx
+++ src/components/commons/PageNotFound.jsx
@@ -1,18 +1,16 @@
 import { Button, Typography } from "neetoui";
-import { useTranslation } from "react-i18next";
+import { Trans } from "react-i18next";
 import { Link } from "react-router-dom";
 
-const PageNotFound = () => {
-  const { t } = useTranslation();
-
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <Typography style="h1">{t("pageNotFound")}</Typography>
-      <Link className="mt-4" to="/">
-        <Button label={t("goBack")} style="primary" />
-      </Link>
-    </div>
-  );
-};
+const PageNotFound = () => (
+  <div className="flex min-h-screen flex-col items-center justify-center">
+    <Typography style="h1">
+      <Trans>pageNotFound</Trans>
+    </Typography>
+    <Link className="mt-4" to="/">
+      <Button label={<Trans>goBack</Trans>} style="primary" />
+    </Link>
+  </div>
+);
 
 export default PageNotFound;
