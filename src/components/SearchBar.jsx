6262f7c50dbe337ac0c7d083bcf3b759debeda83
Rohitsah6372 <91113994+Rohitsah6372@users.noreply.github.com>
2025-05-11 15:54:20 +0530

    Fixed the search bar and lifted up the state

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,53 @@
-const SearchBar = ({ actionBlock }) => (
-  <div className=" flex items-center justify-between  ">{actionBlock}</div>
-);
+import { useEffect, useRef, useState } from "react";
+
+import { Filter, Search } from "neetoicons";
+import { Input } from "neetoui";
+import { useTranslation } from "react-i18next";
+
+const SearchBar = ({ onChange, setCurrentPageNumber, setIsFilterOpen }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const autoInputRef = useRef(null);
+
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.key === "/") {
+        event.preventDefault();
+        autoInputRef.current?.focus();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  const handleChange = e => {
+    const newValue = e.target.value;
+    setSearchTerm(newValue);
+    onChange(newValue);
+    setCurrentPageNumber(1);
+  };
+
+  return (
+    <div className="flex items-center justify-between">
+      <Input
+        className="mx-2 rounded-md border-[#ddd] bg-white p-2"
+        placeholder={t("searchPlaceholder")}
+        prefix={<Search />}
+        ref={autoInputRef}
+        type="Search"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <button onClick={() => setIsFilterOpen(prev => !prev)}>
+        <Filter className="outline-none mr-1 border-none border-[#ddd] p-0.5 hover:text-green-700" />
+      </button>
+    </div>
+  );
+};
 
 export default SearchBar;
